e0587c35f7a43e037e8fe7c2ab7b6c3ccfb1976c
Joshua <smithbro2115@gmail.com>
2019-07-10 10:58:14 -0500

    - Added User Dialog

---
+++ Gui/AddUserDialog.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'D:\Programming\Clocking\Gui\AddUserDialog.ui'
+#
+# Created by: PyQt5 UI code generator 5.11.3
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_AddUserDialog(object):
+    def setupUi(self, AddUserDialog):
+        AddUserDialog.setObjectName("AddUserDialog")
+        AddUserDialog.resize(432, 93)
+        self.gridLayout = QtWidgets.QGridLayout(AddUserDialog)
+        self.gridLayout.setSpacing(2)
+        self.gridLayout.setObjectName("gridLayout")
+        self.lastNameLineEdit = QtWidgets.QLineEdit(AddUserDialog)
+        self.lastNameLineEdit.setObjectName("lastNameLineEdit")
+        self.gridLayout.addWidget(self.lastNameLineEdit, 1, 1, 1, 1)
+        self.firstNameLineEdit = QtWidgets.QLineEdit(AddUserDialog)
+        self.firstNameLineEdit.setObjectName("firstNameLineEdit")
+        self.gridLayout.addWidget(self.firstNameLineEdit, 1, 0, 1, 1)
+        self.label = QtWidgets.QLabel(AddUserDialog)
+        self.label.setObjectName("label")
+        self.gridLayout.addWidget(self.label, 0, 0, 1, 1)
+        self.label_2 = QtWidgets.QLabel(AddUserDialog)
+        self.label_2.setObjectName("label_2")
+        self.gridLayout.addWidget(self.label_2, 0, 1, 1, 1)
+        self.wageLineEdit = QtWidgets.QLineEdit(AddUserDialog)
+        self.wageLineEdit.setObjectName("wageLineEdit")
+        self.gridLayout.addWidget(self.wageLineEdit, 3, 0, 1, 1)
+        self.label_3 = QtWidgets.QLabel(AddUserDialog)
+        self.label_3.setAlignment(QtCore.Qt.AlignLeading|QtCore.Qt.AlignLeft|QtCore.Qt.AlignVCenter)
+        self.label_3.setObjectName("label_3")
+        self.gridLayout.addWidget(self.label_3, 2, 0, 1, 1)
+        self.buttonBox = QtWidgets.QDialogButtonBox(AddUserDialog)
+        self.buttonBox.setOrientation(QtCore.Qt.Horizontal)
+        self.buttonBox.setStandardButtons(QtWidgets.QDialogButtonBox.Cancel|QtWidgets.QDialogButtonBox.Ok)
+        self.buttonBox.setObjectName("buttonBox")
+        self.gridLayout.addWidget(self.buttonBox, 3, 1, 1, 1)
+
+        self.retranslateUi(AddUserDialog)
+        self.buttonBox.accepted.connect(AddUserDialog.accept)
+        self.buttonBox.rejected.connect(AddUserDialog.reject)
+        QtCore.QMetaObject.connectSlotsByName(AddUserDialog)
+        AddUserDialog.setTabOrder(self.firstNameLineEdit, self.lastNameLineEdit)
+        AddUserDialog.setTabOrder(self.lastNameLineEdit, self.wageLineEdit)
+
+    def retranslateUi(self, AddUserDialog):
+        _translate = QtCore.QCoreApplication.translate
+        AddUserDialog.setWindowTitle(_translate("AddUserDialog", "Add User"))
+        self.label.setText(_translate("AddUserDialog", "First Name:"))
+        self.label_2.setText(_translate("AddUserDialog", "Last Name:"))
+        self.label_3.setText(_translate("AddUserDialog", "Default Pay:"))
+
